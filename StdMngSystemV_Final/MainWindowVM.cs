c0bf61e7b05f68af12076854bf3571ca0798319b
SupuniJayasinghe <supuninavodya695@gmail.com>
2023-07-16 16:27:43 +0530

    test01

---
+++ StdMngSystemV_Final/MainWindowVM.cs
@@ -0,0 +1,109 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Desktop01.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Desktop01
+{
+    public  partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public  ObservableCollection<Student> users;
+        [ObservableProperty]
+        public Student selectedUser=null;
+
+
+
+        public void CloseMainWindow()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+
+
+
+        [RelayCommand]
+        public void MsgBox()
+        {
+
+            MessageBox.Show($"{selectedUser.FirstName} GPA value must be between 0 and 4.", "Error");
+        }
+
+        [RelayCommand]
+        public void AddStudent()
+        {
+            var vm = new AddUserVM();
+            vm.title = "ADD USER";
+            AddUserWindow window = new AddUserWindow(vm);
+            window.ShowDialog();
+
+            if (vm.Student.FirstName != null)
+            {
+                users.Add(vm.Student);
+            }
+        }
+
+        [RelayCommand]
+        public void Delete()
+        {
+            if (selectedUser != null)
+            {
+                string name = selectedUser.FirstName;
+                users.Remove(selectedUser);
+                MessageBox.Show($"{name} is Deleted successfuly.", "DELETED \a ");
+                
+            }
+            else
+            {
+                MessageBox.Show("Plese Select Student before Delete.", "Error");
+
+
+            }
+        }
+        [RelayCommand]
+        public void EditStudent()
+        {
+            if (selectedUser != null)
+            {
+                var vm = new AddUserVM(selectedUser);
+                vm.title = "EDIT USER";
+                var window = new AddUserWindow(vm);
+
+                window.ShowDialog();
+
+
+                int index = users.IndexOf(selectedUser);
+                users.RemoveAt(index);
+                users.Insert(index, vm.Student);
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Please Select the student", "Warning!");
+            }
+        }
+
+        public  MainWindowVM()
+        {
+            users = new ObservableCollection<Student>();
+            BitmapImage image1 = new BitmapImage(new Uri("/Model/Images/1.png", UriKind.Relative));
+            users.Add(new Student(12, "Kamal", "Perera", "20/1/1999",image1));
+            BitmapImage image2 = new BitmapImage(new Uri("/Model/Images/2.png", UriKind.Relative));
+            users.Add(new Student(12, "Amali", "Bandara", "12/5/2000",image2));
+            BitmapImage image3 = new BitmapImage(new Uri("/Model/Images/3.png", UriKind.Relative));
+            users.Add(new Student(12, "Supun", "Silva", "6/12/2001",image3));
+            BitmapImage image4= new BitmapImage(new Uri("/Model/Images/4.png", UriKind.Relative));
+            users.Add(new Student(12, "Kasun", "Jayasinghe", "9/7/2001", image4));
+
+        }
+    }
+}
---
+++ StdMngSystemV_Final/Model/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Desktop01.Model
+{
+    public class Student
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public BitmapImage Image { get; set; }  
+
+        public string  DateOfBirth{ get; set; }
+        public Double GPA { get; set; }
+
+        public String ImagePath
+        {
+            get { return $"/Model/Images/{Image}"; }
+        }
+
+        public Student(int age, string firstName, string lastName, string dateOfBirth,BitmapImage  image )
+        {
+            Age = age;
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            Image = image;
+        }
+
+        public Student()
+        {
+        }
+    }
+}
